dbe286fabfb4b7d1b07d442b0b541c12d628850c
franpil12 <francopillajo12@gmail.com>
2026-03-06 16:59:31 -0500

    Arreglo de bugs para railiway

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 ﻿import express from "express";
 import cors from "cors";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { supabase } from "./supabaseClient.js";
