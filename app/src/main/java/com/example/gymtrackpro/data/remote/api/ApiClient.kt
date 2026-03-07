2b1b128709c076b4beaad178a0110ce0360f98dd
franpil12 <francopillajo12@gmail.com>
2026-03-07 13:25:03 -0500

    Adicion con el backend Link

--- app/src/main/java/com/example/gymtrackpro/data/remote/api/ApiClient.kt
+++ app/src/main/java/com/example/gymtrackpro/data/remote/api/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "https://gymtrackpro-production.up.railway.app/"
 
     val api: ApiService by lazy {
         val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
